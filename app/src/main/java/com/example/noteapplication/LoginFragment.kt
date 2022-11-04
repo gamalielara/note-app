8ac0d1ade1c86610380e0b40e560634483d03d4e
ara.boanerges <ara.boanerges@shopee.com>
2022-11-04 19:27:00 +0700

    add RegisterFragment, update UI, set onClick behavior for buttons

--- app/src/main/java/com/example/noteapplication/LoginFragment.kt
+++ app/src/main/java/com/example/noteapplication/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,17 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginButton.setOnClickListener{
+             findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
+        redirectRegisterButton.setOnClickListener{
+            findNavController().navigate((R.id.action_loginFragment_to_registerFragment))
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/noteapplication/RegisterFragment.kt
+++ app/src/main/java/com/example/noteapplication/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_register.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,18 @@ class RegisterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registerButton.setOnClickListener{
+            findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+        }
+
+        redirectLoginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
     }
 
     companion object {
